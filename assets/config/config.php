218c178cafb5d721c6afb671b64c2390de6049d5
hamzah <hamzahnuralfalah@gmail.com>
2025-03-23 21:14:50 +0700

    mayor update

--- assets/config/config.php
+++ assets/config/config.php
@@ -3,6 +3,10 @@
 use Hanafalah\ModuleMedicService\Models as ModuleMedicService;
 
 return [
+    'libs' => [
+        'model' => 'Models',
+        'contract' => 'Contracts'
+    ],
     'database' => [
         'models' => [
             'MedicService' => ModuleMedicService\MedicService::class,
