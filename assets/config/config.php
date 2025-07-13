3063d71161a82850c60c0801d5d225fd2a301fef
Hamzah <hamzahnafalah@gmail.com>
2025-07-14 06:25:57 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -3,20 +3,10 @@
 use Hanafalah\ModuleMedicService as ModuleMedicService;
 
 return [
-<<<<<<< HEAD
-    'libs' => [
-        'model' => 'Models',
-        'contract' => 'Contracts'
-    ],
-    'database' => [
-        'models' => [
-            'MedicService' => ModuleMedicService\MedicService::class,
-=======
     'namespace' => 'Hanafalah\\ModuleMedicService',
     'app' => [
         'contracts' => [
             //ADD YOUR CONTRACTS HERE
->>>>>>> 8174230b31edc09e4a6dc4f051e324df08ff0c0a
         ]
     ],
     'libs' => [
